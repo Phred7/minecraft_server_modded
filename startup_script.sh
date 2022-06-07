66a4218dfc01d0321c4adfa3a246062c4ea5db0c
Phred7 <woward00@gmail.com>
2022-06-07 14:03:56 -0700

    automatic backup.

--- startup_script.sh
+++ startup_script.sh
@@ -6,7 +6,10 @@ while [ $a -lt 10 ]
 do
 	#/usr/lib/jvm/java-8-openjdk-amd64/bin/java -Xms2G -Xmx8G -jar forge-1.16.5-36.2.23.jar nogui
 	#/usr/lib/jvm/java-8-openjdk-amd64/bin
-	/usr/lib/jvm/java-8-openjdk-amd64/bin/java -Xms16G -Xmx16G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar forge-1.16.5-36.2.23.jar nogui
+	# flags src: https://aikar.co/mcflags.html
+	# forge install src: https://linuxnorth.org/minecraft/modded_linux.html
+	
+	/usr/lib/jvm/java-8-openjdk-amd64/bin/java -Xms14G -Xmx14G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Xloggc:gc.log -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=1M -jar forge-1.16.5-36.2.34.jar nogui
 	#java -Xms2G -Xmx8G -jar forge-1.16.5-36.2.23.jar nogui
 	echo Backing-up
 	python3 backup_server.py
